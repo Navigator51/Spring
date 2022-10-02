3d48ce4dad4f9532b74cb33b87241e51eee4056c
markov.me <markov.me@esphere.ru>
2022-10-02 23:17:35 +0300

    Починил + TODO

--- src/main/java/su/goodcat/spring/services/impl/InvitationServiceImpl.java
+++ src/main/java/su/goodcat/spring/services/impl/InvitationServiceImpl.java
@@ -1,20 +1,20 @@
 package su.goodcat.spring.services.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.mapstruct.factory.Mappers;
 import org.springframework.stereotype.Service;
 import su.goodcat.spring.domain.docproject.Invitation;
 import su.goodcat.spring.domain.docproject.InvitationStatus;
 import su.goodcat.spring.domain.docproject.User;
 import su.goodcat.spring.domain.dto.UserInvitatorDTO;
 import su.goodcat.spring.exceptions.HimselfInvitationException;
+import su.goodcat.spring.mapper.UserInvitatorMapper;
 import su.goodcat.spring.repositories.InvitationRepository;
 import su.goodcat.spring.repositories.UserRepository;
 import su.goodcat.spring.services.interfaces.InvitationService;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Service
@@ -35,20 +35,22 @@ public class InvitationServiceImpl implements InvitationService {
 
     @Override
     public List<UserInvitatorDTO> getInvitationsByUserId(Long userId) {
+        UserInvitatorMapper mapper = Mappers.getMapper(UserInvitatorMapper.class);
         List<Invitation> invitationList = invitationRepository.getInvitationsByRecipientIdAndStatus(userId, InvitationStatus.SENT);
         List<Long> listSenderId = invitationList.stream()
                 .map(Invitation::getSenderId)
                 .distinct()
                 .toList();
         List<User> userList = userRepository.findAllById(listSenderId);
-        Map<Invitation, User> userMap = invitationList.stream()
-                .collect(Collectors.toMap(Function.identity(),
-                        invitation -> userList
-                                .stream()
+        // TODO сделать супермегаоптимальный алгоритм через toMap и HashMap
+        return invitationList.stream()
+                .map(invitation -> mapper.fromUserToUserInvitatorDTO(
+                        userList.stream()
                                 .filter(user -> user.getUserId() == invitation.getSenderId())
-                                .findFirst().orElseThrow()));
-        System.out.println(userMap);
-//        Mappers.getMapper(UserInvitatorMapper.class).fromUserToUserInvitatorDTO();
-        return null;
+                                .findFirst()
+                                .orElseThrow(),
+                        invitation
+                ))
+                .collect(Collectors.toList());
     }
 }
